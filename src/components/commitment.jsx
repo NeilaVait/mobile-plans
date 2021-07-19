ef34bbd9428bf7e5002f4cd926af299fa4836ab9
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-19 11:58:43 +0300

    commitment html

--- src/components/commitment.jsx
+++ src/components/commitment.jsx
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Commitment extends Component {
   render() {
-    return <h2>commitment</h2>;
+    return (
+      <div className="commitment-component">
+        <div className="c__selection">
+          <span className="c__select"></span>
+          <span className="c__select-title">24 men.</span>
+        </div>
+        <div className="c__selection">
+          <span className="c__select"></span>
+          <span className="c__select-title">Be isipareigojimu</span>
+        </div>
+      </div>
+    );
   }
 }
 
