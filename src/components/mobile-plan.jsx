e8ba48847219a4f60deec2485bb4176e94c94ccf
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-19 10:26:40 +0300

    mobile plan html part

--- src/components/mobile-plan.jsx
+++ src/components/mobile-plan.jsx
@@ -4,7 +4,22 @@ class MobilePlan extends Component {
   state = {};
 
   render() {
-    return <div className="mobile-plan">mobile plan</div>;
+    return (
+      <div className="mobile-plan">
+        <h6 className="plan__header">Mobilus START 1</h6>
+        <h1 className="plan__dataAllow">1 GB</h1>
+        <small className="plan__dataEu">Iš jų 1 GB ES/EEE</small>
+        <p className="plan__min-sms">Neribotos MIN ir SMS</p>
+        <div className="plan__features">sarasas</div>
+        <div className="plan__bottom">
+          <div className="bottom__price-part">
+            <h2>9,90 €/mėn.</h2>
+            <small>24 mėn. sutartis</small>
+          </div>
+          <button className="plan__cta">Domina</button>
+        </div>
+      </div>
+    );
   }
 }
 
